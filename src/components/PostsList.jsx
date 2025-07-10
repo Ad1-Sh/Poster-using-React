3880fa12285c461d3c052f3b8149e65dbe63d5d1
Aditya Sharma <hi.aditya.in@gmail.com>
2025-07-10 19:14:30 +0530

    Linking and Navigation

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,37 +1,22 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import { useEffect, useState } from "react";
-
-
-
+import { useLoaderData } from "react-router-dom";
 
 function PostsList() {
 
-    const [posts, setPosts] = useState([]);
-    const [isFetching, setIsFetching] = useState(false);
+    const posts = useLoaderData();
+  
+    // const [isFetching, setIsFetching] = useState(false);
 
-    useEffect(() => {
-        async function fetchPosts(){
-            setIsFetching(true);
-        const response = await fetch('http://localhost:8080/posts')
-        const resData = await response.json();
-        setPosts(resData.posts);
-        setIsFetching(false);
-        }       
-        fetchPosts();
-    }, []);
+    // useEffect(() => {
+    //     async function fetchPosts(){
+    //         setIsFetching(true);
+    //     setPosts(resData.posts);
+    //     setIsFetching(false);
+    //     }       
+    //     fetchPosts();
+    // }, []);
 
-
-    function addPostHandler(postData) {
-        fetch('http://localhost:8080/posts',{
-            method: 'POST',
-            body: JSON.stringify(postData),
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        });
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
 
     return (
         <>
@@ -40,23 +25,17 @@ function PostsList() {
                  onCancel={onStopPosting}
                  onAddPost={addPostHandler}/>
         </Modal> : null} */}
-        {!isFetching && posts.length > 0 && (
+        {posts.length > 0 && (
         <ul className={classes.posts}>
-            {posts.map((post) => <Post key={post.body} author={post.author} body={post.body}/>)}
+            {posts.map((post) => <Post key={post.id} id={post.id} author={post.author} body={post.body}/>)}
         </ul>
         )}
-        {!isFetching && posts.length === 0 && (
+        {posts.length === 0 && (
             <div style={{ textAlign: 'center', color:'white' }}>
                 <h2>No posts yet</h2>
                 <p>Start adding some!</p>
             </div>
         )}
-        {isFetching && (
-            <div style={{textAlign:'center', color: 'white'}}>
-            <p>Loading Posts...</p>
-            </div>
-        )
-        }
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Posts from './routes/Posts.jsx'
+import Posts, {loader as postsLoader} from './routes/Posts.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import RootLayout from './routes/RootLayout.jsx'
-import NewPost from './routes/NewPost.jsx'
+import NewPost, {action as newPostAction} from './routes/NewPost.jsx'
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,7 +15,11 @@ const router = createBrowserRouter([
       {
       path: '/',
       element: <Posts />,
-      children:[{path: '/create-post', element: <NewPost />}], },
+      loader: postsLoader,
+      children:[
+        {path: '/create-post', element: <NewPost />, action: newPostAction},
+        {path: '/:id', element: <PostDetails />, loader: postDetailsLoader }
+    ], },
     ],
   },
 ]);
